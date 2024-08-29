87dda7bafcd796561f55ee32240403c1cc540eb6
Willyan Costa <willyancr@gmail.com>
2024-08-28 22:08:48 -0300

    chore: update components and styles in HeroPage and globals.css
    - Replace PopularProperties with PopularPropertiesSection and add DreamHomeSection
    - Update CSS variables and styles in globals.css for light and dark themes

--- src/app/(lading-page)/(hero)/page.tsx
+++ src/app/(lading-page)/(hero)/page.tsx
@@ -1,11 +1,13 @@
-import PopularProperties from "@/app/components/popular-properties";
+import PopularPropertiesSection from "@/app/components/popular-properties-section";
 import Home from "../home/page";
+import DreamHomeSection from "@/app/components/dream-home-section";
 
 export default function HeroPage() {
   return (
     <div>
       <Home />
-      <PopularProperties />
+      <PopularPropertiesSection />
+      <DreamHomeSection />
     </div>
   );
 }
