1387dfca709c7252a3aa71d8dfa3f547f53c44b3
Willyan Costa <willyancr@gmail.com>
2024-08-30 11:08:10 -0300

    feat: Add new components for property search functionality
    - Added new components for property search functionality including InputSearchState, Button, and Select to improve user experience and interaction with property search features.

---
+++ src/app/(lading-page)/imoveis/page.tsx
@@ -0,0 +1,36 @@
+import Header from "@/app/components/header";
+import InputSearchState from "@/app/components/input-search-state";
+import Image from "next/image";
+
+export default function Imoveis() {
+  return (
+    <div className="h-screen">
+      <Header />
+      <div className="relative mx-auto w-full max-w-[1200px]">
+        <Image
+          src="/bg-imoveis.png"
+          alt="Logo"
+          width={1200}
+          height={1200}
+          quality={100}
+          className="mx-auto h-[800px] w-full object-cover brightness-50"
+        />
+        <div className="absolute inset-0 mx-auto flex h-full w-full flex-col items-center justify-center gap-20 text-5xl text-zinc-50">
+          <div className="flex items-center gap-6">
+            <Image
+              src="/logo-componente.png"
+              alt="Logo"
+              width={70}
+              height={70}
+              quality={100}
+            />
+            <h1 className="font-CoHeadlineBold">
+              Encontre seu imovél favorito
+            </h1>
+          </div>
+          <InputSearchState />
+        </div>
+      </div>
+    </div>
+  );
+}
