a40bf834c5e1bfe2aac1b7248750e16f72dc5574
Willyan Costa <willyancr@gmail.com>
2024-09-04 14:54:12 -0300

    chore: update MetricsSummary component styles and content
    Refactor MetricsSummary component to use smaller gap and updated styles for
    metric boxes. Also, adjust the displayed metrics and descriptions for better
    clarity and consistency.

--- src/app/components/metrics-summary.tsx
+++ src/app/components/metrics-summary.tsx
@@ -1,21 +1,21 @@
 export default function MetricsSummary() {
   return (
-    <div className="flex gap-12">
-      <div className="flex flex-col">
+    <div className="flex gap-2">
+      <div className="flex flex-col bg-primary/30 px-3 py-2 text-sm rounded-lg w-28">
         <p className="text-lg font-semibold">
           500<span className="text-zinc-950">+</span>
         </p>
         <span className="text-xs">Imóveis</span>
         <span className="text-xs">anunciados</span>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col bg-primary/30 px-3 py-2 text-sm rounded-lg w-28">
         <p className="text-lg font-semibold">
           500<span className="text-zinc-950">+</span>
         </p>
         <span className="text-xs">Famílias</span>
         <span className="text-xs">felizes</span>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col bg-primary/30 px-3 py-2 text-sm rounded-lg w-28">
         <p className="text-lg font-semibold">
           09<span className="text-zinc-950">+</span>
         </p>
