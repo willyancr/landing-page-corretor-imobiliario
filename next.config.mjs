5ff59705a61e66ce816b3a0494f097c02bc8aab0
Willyan Costa <willyancr@gmail.com>
2024-09-16 21:42:37 -0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // domains: ["127.0.0.1"],
+    domains: ["project-stripe-artur.onrender.com"],
+  },
+};
 
 export default nextConfig;
